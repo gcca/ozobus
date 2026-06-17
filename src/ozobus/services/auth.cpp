cb7ef5d97550733af83e982001871362f6461d48
gcca <git@gcca.dev>
2026-06-17 01:04:46 -0500

    20260617010446

--- src/ozobus/services/auth.cpp
+++ src/ozobus/services/auth.cpp
@@ -6,6 +6,7 @@ grpc::Status AuthServiceImpl::UserDetails(
     grpc::ServerContext* /*context*/,
     const auth::UserDetailsRequest* request,
     auth::UserDetailsResponse* response) {
+
   response->set_user_id("usr-001");
   response->set_username(request->username());
   response->set_email(request->username() + "@example.com");
@@ -13,6 +14,7 @@ grpc::Status AuthServiceImpl::UserDetails(
   response->add_roles("viewer");
   response->set_created_at(1718582400);  // 2024-06-17 00:00:00 UTC
   response->set_is_active(true);
+
   return grpc::Status::OK;
 }
 
